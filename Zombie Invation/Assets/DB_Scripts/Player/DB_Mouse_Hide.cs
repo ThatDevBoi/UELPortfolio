34c947a82f44bae70859a20a22ea86e8236703bf
davebrooks199909@gmail.com <davebrooks199909@gmail.com>
2018-10-25 19:10:04 +0100

    Zombie Invasion
    A 3D Zombie game thati abandoned, however using C# i made a Player movement script, Gun shooting script and Weapon holder script

---
+++ Zombie Invation/Assets/DB_Scripts/Player/DB_Mouse_Hide.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DB_Mouse_Hide : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}  
+
+
+    
